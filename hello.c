30013131f2a73255f5669bf82a169e1bb4a64592
AyomideOludipe <54687427+AyomideOludipe@users.noreply.github.com>
2019-09-11 14:48:18 -0500

    Create hello.c
    I added a line to the code and this program is about my name and major.

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Ayo Oludipe
+ * Date: 9 september, 2019
+
+ *this program prints my name and major
+ */
+
+ #include<stdio.h>
+ #include<stdlib.h>
+
+ int main(int argc, char **argv){
+   printf("Ayomide Oludipe\n");
+   printf("computer Science\n");
+   return 0;
+ }
